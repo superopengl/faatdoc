24872215443d1ace5959b6845f8655ed1c9e74f4
Jun Shao <jshao@securecodewarrior.com>
2020-09-24 13:20:37 +1000

    bugfix

--- aua-web/src/util/varNameToLabelName.js
+++ aua-web/src/util/varNameToLabelName.js
@@ -1,14 +1,16 @@
 
+import {capitalize} from 'lodash';
+
 export function varNameToLabelName(camelCase) {
   try {
     const bigCase = camelCase?.toUpperCase();
-    for(const b of ['TFN', 'ABN', 'ACN']) {
-      if(b === bigCase){
-        return b;
-      }
+    if(['TFN', 'ABN', 'ACN'].includes(bigCase)) {
+      return bigCase;
     }
+
     const spaceSplitted = camelCase?.replace(/([A-Z])/g, ' $1');
-    return spaceSplitted.charAt(0).toUpperCase() + spaceSplitted.substring(1);
+    const words = spaceSplitted.split(/ +/g).map(capitalize);
+    return words.join(' ');
   } catch (e) {
     debugger;
   }
