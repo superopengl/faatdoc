dc7b145d05d9e92a1a0e1f3a1023be17f8484261
Jun Shao <jshao@securecodewarrior.com>
2020-10-19 10:59:59 +1100

    bugfix

--- aua-app/src/utils/guessDisplayNameFromFields.ts
+++ aua-app/src/utils/guessDisplayNameFromFields.ts
@@ -2,7 +2,7 @@ import * as _ from 'lodash';
 
 export function guessDisplayNameFromFields(fields) {
   const fieldMap = _.keyBy(fields || [], 'name');
-  const [givenName, surname, company] = ['givenName', 'surname', 'company'].map(k => fieldMap[k]?.value || '');
+  const [givenName, surname, company] = ['Given_Name', 'Surname', 'Company'].map(k => fieldMap[k]?.value || '');
   const fullname = `${givenName} ${surname}`.trim();
   return fullname || company || 'Unnamed';
 }
